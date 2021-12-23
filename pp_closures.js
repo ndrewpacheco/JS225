f9133be4543cf455a3b140d9093c42753445a02c
Andrew  Pacheco <ndrewpacheco@gmail.com>
2021-12-22 23:24:28 -0500

    workoing on closures

---
+++ pp_closures.js
@@ -0,0 +1,46 @@
+// // function makeMultipleLister(multiple) {
+// //   return function() {
+// //     for (let num = multiple; num < 100; num += multiple) {
+// //       console.log(num);
+// //     }
+// //   }
+// // }
+
+// // let lister = makeMultipleLister(13);
+// // lister();
+// // // 13
+// // // 26
+// // // 39
+// // // 52
+// // // 65
+// // // 78
+// // // 91
+
+
+// let total = 0;
+
+// function add(number) {
+//   return total += number;
+// }
+// function subtract(number) {
+//   return total -= number;
+// }
+
+// add(1);
+// // 1
+// add(42);
+// // 43
+// subtract(39);
+// // 4
+// add(6);
+// // 10
+
+function startup() {
+  let status = 'ready';
+  return function() {
+    console.log('The system is ready.');
+  };
+}
+
+let ready = startup();
+let systemStatus = // ?
--- test.js
+++ test.js
@@ -1,8 +1,47 @@
-let numbers = [1, 2, 3, 4];
-function makeCheckEven() {
-  // ... implement this function
+function makeList() {
+  let todoList = [];
+
+  return function(todo) {
+    if (todo) {
+      if (todoList.includes(todo)) {
+        let i = todoList.indexOf(todo);
+        todoList.splice(i, 1);
+        console.log(todo + ' removed!')
+      } else {
+        todoList.push(todo);
+        console.log(todo + ' added!')
+
+      }
+    } else {
+      if (todoList.length === 0) {
+        console.log('The list is empty');
+      } else {
+        todoList.forEach(entry => console.log(entry));
+
+      }
+    }
+  }
 }
 
-let checkEven = makeCheckEven();
+/*
+When called with an argument that is not already on the list, it adds that argument to the list.
 
-numbers.filter(checkEven); // [2, 4]
+When called with an argument that is already on the list, it removes the element from the list.
+
+When called without arguments, it logs all items on the list. If the list is empty,
+it logs an appropriate message.
+*/
+let list = makeList();
+list();
+// The list is empty.
+list('make breakfast');
+// make breakfast added!
+list('read book');
+// read book added!
+list();
+// make breakfast
+// read book
+list('make breakfast');
+// make breakfast removed!
+list();
+// read book
