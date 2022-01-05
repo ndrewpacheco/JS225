d0dbbf74decf415fa716046828f6cf357861ada1
Andrew  Pacheco <ndrewpacheco@gmail.com>
2022-01-05 12:24:29 -0500

    figuring out vscode

--- object_creation/practice.js
+++ object_creation/practice.js
@@ -1,54 +1,8 @@
-function createInvoice(services = {}) {
-  let amountPaid = 0;
-  return {
-    phone: services.phone || 3000,
-    internet: services.internet || 5500,
-    total: function() {
-      return this.phone + this.internet;
-    },
+let prot = {};
 
-    addPayment(payment) {
-      amountPaid += payment.total();
-    },
+let foo = Object.create(prot);
 
-    addPayments(payments) {
-      payments.forEach(payment => this.addPayment(payment));
-    },
-    amountDue() {
-      return console.log(this.total() - amountPaid);
-    },
-  };
-}
+Object.getPrototypeOf(foo) === prot;
 
-function createPayment(services = {}) {
-  return {
-    internet: services.internet || 0,
-    phone: services.phone || 0,
-    amount: services.amount,
-    total() {
-      return this.amount || this.internet + this.phone;
-    },
-  };
-}
+foo.isPrototypeOf(prot);
 
-let invoice = createInvoice({
-  phone: 1200,
-  internet: 4000,
-});
-
-let payment1 = createPayment({
-  amount: 2000,
-});
-
-let payment2 = createPayment({
-  phone: 1000,
-  internet: 1200,
-});
-
-let payment3 = createPayment({
-  phone: 1000,
-});
-
-invoice.addPayment(payment1);
-invoice.addPayments([payment2, payment3]);
-invoice.amountDue();       // this should return 0
